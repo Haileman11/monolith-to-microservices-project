5bc58cd552fd80310a97106d092e254afa8e06cc
Hailegabriel Mekonnen <haile.mak11@gmail.com>
2022-09-04 12:55:22 +0300

    add deployment files,add service files,add config files

---
+++ set_deployment.sh
@@ -0,0 +1,16 @@
+# Apply env variables and secrets
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-config.yaml
+# Deployments - Double check the Dockerhub image name and version in the deployment files
+kubectl apply -f udagram-api-feed-deployment.yaml
+kubectl apply -f udagram-api-user-deployment.yaml
+kubectl apply -f udagram-frontend-deployment.yaml
+kubectl apply -f reverseproxy-deployment.yaml
+# Do the same for other three deployment files
+# Service
+kubectl apply -f udagram-api-feed-service.yaml
+kubectl apply -f udagram-api-user-service.yaml
+kubectl apply -f udagram-frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
+# Do the same for other three service files
